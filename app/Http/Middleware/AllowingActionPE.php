25ba26e94b7832e3e2c1d8a0ff2a8c6f40492c09
hunter3d <alex.bolshakov@gmail.com>
2023-03-15 13:05:54 +0200

    lang

--- app/Http/Middleware/AllowingActionPE.php
+++ app/Http/Middleware/AllowingActionPE.php
@@ -6,21 +6,18 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AllowingActionPE
-{
+class AllowingActionPE {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, $ability)
-    {
-        if (!Auth::user()->can($ability))
-        {
-            return back()->with('error',"У вас нет разрешения для выполнения этого действия");
+    public function handle( Request $request, Closure $next, $ability ) {
+        if ( !Auth::user()->can( $ability ) ) {
+            return back()->with( 'error', "У вас нет разрешения для выполнения этого действия" );
         }
-        return $next($request);
+        return $next( $request );
     }
 }
