417ff3877d5271e16732b953594d585420a75b93
hunter3d <alex.bolshakov@gmail.com>
2023-03-21 12:00:45 +0200

    events routes

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -28,7 +28,7 @@
                                 <th class="text-nowrap" scope="col">@sortablelink('start','Начало')</th>
                                 <th class="text-nowrap" scope="col">@sortablelink('start','Конец')</th>
 
-                                <th class="text-nowrap" scope="col">@sortablelink('pay_uah','<i class="fal fa-file"></i>')</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('pay_uah',new Illuminate\Support\HtmlString('<i class="fal fa-file"></i>'))</th>
                                 <th class="text-nowrap" scope="col">@sortablelink('price_uah','Грн')</th>
                                 <th class="text-nowrap" scope="col">@sortablelink('price_euro','Евро')</th>
                                 <th class="text-nowrap" scope="col">@sortablelink('price_usd','Usd')</th>
