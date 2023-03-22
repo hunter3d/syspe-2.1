b2ad9f808802058168523b0f0a052b1e1ace9fc1
hunter3d <alex.bolshakov@gmail.com>
2023-03-22 14:23:09 +0200

    tickets

--- resources/views/tickets/event.blade.php
+++ resources/views/tickets/event.blade.php
@@ -35,35 +35,43 @@
                     <div class="table-responsive">
                         <table class="table table-hover table-sm">
                             <thead>
-                                <tr>
-                                    <th class="text-nowrap" scope="col">@sortablelink('id','#')</th>
-                                    <th class="text-nowrap" scope="col">@sortablelink('visitor.email','Посетитель')</th>
-                                    <th>ФИО</th>
-                                    <th>Компания</th>
-                                    <th>Должность</th>
-                                    <th style="text-align: center"><i class="fal fa-fw fa-brackets"></i></th>
-                                    <th></th>
-                                    <th class="text-nowrap" scope="col">@sortablelink('event.name_ru','Мероприятие')</th>
-                                    <th class="text-nowrap" scope="col">@sortablelink('code','Штрихкод')</th>
-                                    <th class="text-nowrap" style="text-align: center" scope="col"><i class="fal fa-fw fa-barcode-read"></i></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
+                            <tr>
+                                <th class="text-nowrap" scope="col">@sortablelink('id','#')</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('visitor.email','Посетитель')</th>
+                                <th class="text-nowrap" scope="col">ФИО</th>
+                                <th class="text-nowrap" scope="col">Компания</th>
+                                <th class="text-nowrap" scope="col">Должность</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('order.pay_method','Способ оплаты')</th>
+                                <th class="text-nowrap" scope="col">Промокод</th>
+                                <th class="text-nowrap" scope="col">Описание</th>
+                                <th class="text-nowrap" scope="col">Валюта</th>
+                                <th class="text-nowrap" scope="col">Цена</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('exhibition.name','Выставка')</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('event.name_ru','Мероприятие')</th>
+                                <th class="text-nowrap" scope="col">@sortablelink('code','Штрихкод')</th>
+                                <th class="text-nowrap" style="text-align: center" scope="col"><i class="fal fa-fw fa-barcode-read"></i></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
                             </thead>
                             <tbody>
                             @foreach( $tickets as $ticket )
                                 <tr>
                                     <td>{{ $ticket->id }}</td>
-                                    <td>{{ $ticket->visitor->email }}</td>
-                                    <td>{{ $ticket->visitor->card?$ticket->visitor->card->last_name.' '.$ticket->visitor->card->first_name.' '.$ticket->visitor->card->second_name:'' }}</td>
-                                    <td>{{ $ticket->visitor->card?$ticket->visitor->card->company:'' }}</td>
-                                    <td>{{ $ticket->visitor->card?$ticket->visitor->card->position:'' }}</td>
+                                    <td>{{ $ticket->visitor?$ticket->visitor->email:'---' }}</td>
+                                    <td>{{ $ticket->visitor&&$ticket->visitor->card?$ticket->visitor->card->last_name.' '.$ticket->visitor->card->first_name.' '.$ticket->visitor->card->second_name:'' }}</td>
+                                    <td>{{ $ticket->visitor&&$ticket->visitor->card?$ticket->visitor->card->company:'' }}</td>
+                                    <td>{{ $ticket->visitor&&$ticket->visitor->card?$ticket->visitor->card->position:'' }}</td>
+                                    <td>{{ $ticket->order?$ticket->order->pay_method:'' }}</td>
                                     <td>{{ $ticket->order&&$ticket->order->promocode?$ticket->order->promocode->code:'' }}</td>
                                     <td>{{ $ticket->order&&$ticket->order->promocode?$ticket->order->promocode->description:'' }}</td>
+                                    <td>{{ $ticket->order&&$ticket->order->currency?$ticket->order->currency->name_ru:'' }}</td>
+                                    <td>{{ $ticket->order?$ticket->order->price:'' }}</td>
+                                    <td>{{ $ticket->exhibition->name }}</td>
                                     <td>{{ $ticket->event->name_ru }}</td>
                                     <td>{{ $ticket->code }}</td>
                                     <td class="{{$ticket->checked_at?'table-success':''}}">{{ $ticket->checked_at }}</td>
-                                    <td><a href="{{ route('cards.show',['id'=>($ticket->visitor->card?$ticket->visitor->card->id:0)]) }}" title="Посмотреть карточку"><i class="fal fa-fw fa-address-card"></i></a></td>
+                                    <td><a href="{{ route('cards.show',['id'=>($ticket->visitor&&$ticket->visitor->card?$ticket->visitor->card->id:0)]) }}" title="Посмотреть карточку"><i class="fal fa-fw fa-address-card"></i></a></td>
                                     <td><a href="{{ $ticket->file }}" title="Скачать / Посмотреть билет"><i class="fal fa-fw fa-file-pdf"></i></a></td>
                                 </tr>
                             @endforeach
