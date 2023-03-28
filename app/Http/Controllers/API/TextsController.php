57eb4f2ced702a180b9ef3b0c4d411cfd9f6d08d
hunter3d <alex.bolshakov@gmail.com>
2023-03-28 15:44:31 +0300

    texts controller

--- app/Http/Controllers/API/TextsController.php
+++ app/Http/Controllers/API/TextsController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\App;
 class TextsController extends Controller {
     public function show( $id ) {
         $lang = App::currentLocale();
-        $text = Texts::select('id','name','text_'.$lang.' AS text')->where('id',$id)->first();
+        $text = Texts::select('text_'.$lang.' AS text')->where('id',$id)->first();
         return response()->json([
             'status' => true,
-            'text' => htmlspecialchars_decode($text),
+            'text' => htmlspecialchars_decode($text->text),
         ]);
     }
 }
