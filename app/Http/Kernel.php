417633df7dd5384211bef6493efe1324f2d69f03
alex <alex.bolshakov@gmail.com>
2023-04-09 00:32:01 +0300

    api2 routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,12 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'api2' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
     ];
 
     /**
